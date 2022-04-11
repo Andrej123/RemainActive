965d68599e402eafe0aa381f014c0055eb2468cd
Andrej Krizmancic <andrej.krizmancic@gmail.com>
2022-04-11 15:37:27 +0200

    Initial Commit

---
+++ RemainActive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RemainActive
+//
+//  Created by Andrej Krizmancic on 11/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
